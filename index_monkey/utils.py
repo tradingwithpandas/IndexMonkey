f01d4579973b9a09f69cf3222f6eb9ef2ced4e75
tradingwithpandas <tradingwithpandas@gmail.com>
2022-09-19 13:46:57 +0100

    Add date period ago util functions

--- index_monkey/utils.py
+++ index_monkey/utils.py
@@ -1,4 +1,5 @@
 import datetime
+from pandas.tseries.offsets import BDay
 
 
 def get_first_biz_day_of_month(eff_date):
@@ -21,7 +22,7 @@ def get_first_biz_day_of_month(eff_date):
 
 
 def check_valid_month(month):
-    if month and month < 1 or month > 12:
+    if month is not None and month < 1 or month > 12:
         raise ValueError(f'Month should be between 1 and 12. You passed {month}')
 
 
@@ -38,3 +39,36 @@ def daterange(start_date, end_date, ignore_weekends=False):
         if eff_date.isoweekday() not in (6, 7):
             yield eff_date
         eff_date = eff_date + datetime.timedelta(days=1)
+
+
+DAY_AGO_BIZ_DAYS = -1
+WEEK_AGO_BIZ_DAYS = -5
+MONTH_AGO_BIZ_DAYS = -20
+
+
+def get_date_from_n_days_ago(eff_date, n_days, biz_day_mode=True):
+    if biz_day_mode:
+        new_date = (eff_date + BDay(n_days)).to_pydatetime().date()
+    else:
+        new_date = eff_date + datetime.timedelta(days=n_days)
+    return new_date
+
+
+def day_ago(eff_date, biz_day_mode=True):
+    return get_date_from_n_days_ago(eff_date, DAY_AGO_BIZ_DAYS, biz_day_mode=biz_day_mode)
+
+
+def week_ago(eff_date, biz_day_mode=True):
+    return get_date_from_n_days_ago(eff_date, WEEK_AGO_BIZ_DAYS, biz_day_mode=biz_day_mode)
+
+
+def month_ago(eff_date, biz_day_mode=True):
+    return get_date_from_n_days_ago(eff_date, MONTH_AGO_BIZ_DAYS, biz_day_mode=biz_day_mode)
+
+
+def parse_date(date_str, date_format='%Y-%m-%d'):
+    try:
+        parsed_date = datetime.datetime.strptime(date_str, date_format).date()
+    except ValueError:
+        parsed_date = None
+    return parsed_date
