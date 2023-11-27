37eb0367b5f6456eb7dab955ee906d54fa5b7b21
tradingwithpandas <tradingwithpandas@gmail.com>
2023-11-27 23:57:25 +0000

    New classes and connections for Investment Trust scrapers

--- index_monkey/model.py
+++ index_monkey/model.py
@@ -1,13 +1,16 @@
+import sqlite3
+
 from sqlalchemy import create_engine
 from sqlalchemy import Column, ForeignKey, BigInteger, Date, Float, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
-from .constants import DB_NAME
+from .constants import DB_NAME, NEW_DB_NAME
 
 engine = create_engine('sqlite:///{}?check_same_thread=False'.format(DB_NAME), pool_pre_ping=True)
-Base = declarative_base(bind=engine)
+new_engine = create_engine('sqlite:///{}?check_same_thread=False'.format(NEW_DB_NAME), pool_pre_ping=True)
+Base = declarative_base()
 Session = scoped_session(sessionmaker(bind=engine, autoflush=False))
-
+CON = sqlite3.connect(NEW_DB_NAME)
 
 def session():
     s = sessionmaker(bind=engine)
@@ -76,3 +79,38 @@ class ETFStats(Base):
     nav = Column(Float)
     shares_outstanding = Column(Float)
     aum = Column(Float)
+
+
+class InvTrustHoldings(Base):
+    __tablename__ = 'inv_trust_holdings'
+    hdate = Column(Date, nullable=False, primary_key=True)
+    trust = Column(String, nullable=False, primary_key=True)
+    name = Column(String, nullable=False, primary_key=True)
+    ticker = Column(String, nullable=False, primary_key=True)
+    weight = Column(Float)
+    qty = Column(Float)
+
+
+class InvTrustStats(Base):
+    __tablename__ = 'inv_trust_stats'
+    sdate = Column(Date, nullable=False, primary_key=True)
+    ticker = Column(String, nullable=False, primary_key=True)
+    sector = Column(String)
+    total_assets = Column(Float)
+    mkt_cap = Column(Float)
+    nav = Column(Float)
+    close = Column(Float)
+    gearing = Column(Float)
+    charge = Column(Float)
+    charge_last_review = Column(String)
+    div_yield = Column(Float)
+    shs_outstanding = Column(Float)
+    tsy_shs = Column(Float)
+
+
+class InvTrustBreakdowns(Base):
+    __tablename__ = 'inv_trust_static'
+    ticker = Column(String, nullable=False, primary_key=True)
+    name = Column(String, nullable=False)
+    sector = Column(String)
+
